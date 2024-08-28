eccc8977554b89dc691b2c67e02dd2f74801cb9a
ESX <evgeniy_h@list.ru>
2024-08-28 12:42:07 +1000

    initial commit with error

---
+++ school/views.py
@@ -0,0 +1,20 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    student = Student.objects.all() #.order_by('group')
+    teacher = Teacher.objects.all()
+    context = {
+        'student': student,
+        'teacher': teacher,
+    }
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
